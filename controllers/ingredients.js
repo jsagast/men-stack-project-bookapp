0c8e2f4d7a30991e8f164d3a7c6f968b0daff448
jsagast <143118875+jsagast@users.noreply.github.com>
2025-12-12 10:10:34 -0700

    Initial commit

---
+++ controllers/ingredients.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+const User = require('../models/user.js');
+const Recipe = require('../models/recipe.js');
+const Ingredient=require('../models/ingredient.js')
+
+router.get('/', async (req, res) => {
+    const ingredients=await Ingredient.find()
+    res.render('ingredients/index-new.ejs',{ingredients: ingredients});
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const exists= await Ingredient.findOne({ name: req.body.name });
+    !exists? await Ingredient.create(req.body): null;
+    // await Ingredient.create(req.body);
+    res.redirect('/ingredients');
+  } catch (error) {
+    console.log(error);
+    res.redirect('/ingredients');
+  }
+});
+
+
+module.exports = router;
+
