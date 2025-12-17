ed34258577573667d701b5385c947d86edb7c7f3
Jose Sagastume <jose.sagast@gmail.com>
2025-12-17 13:37:16 -0700

    working app

--- controllers/users.js
+++ controllers/users.js
@@ -1,27 +1,25 @@
 const express = require('express')
 const router = express.Router()
 const Book= require('../models/book.js')
+const BookClub=require('../models/bookclub.js')
 
 //Profile Main Page
 router.get('/profile', async (req, res) => {
     try {
     
-        const myShelf = await Book.find({
-            pulledByUsers: req.session.user._id,
-        }).populate('owner');
+        const myShelf = await Book.find({pulledByUsers: req.session.user._id}).populate('owner');
 
-        const myBooks = await Book.find({
-            owner: req.session.user._id,
-        }).populate('owner');
+        const myBooks = await Book.find({owner: req.session.user._id}).populate('owner');
 
-        const myFavoriteBooks = await Book.find({
-            favoritedByUsers: req.session.user._id,
-        }).populate('owner');
+        const myFavoriteBooks = await Book.find({favoritedByUsers: req.session.user._id}).populate('owner');
+
+        const myBookClubs=await BookClub.find({members:req.session.user._id,}).populate ('owner');
         
         res.render('profile/show.ejs', {
             myShelf,
             myBooks,
             myFavoriteBooks,
+            myBookClubs,
         })
     } catch (error) {
         console.log(error)
